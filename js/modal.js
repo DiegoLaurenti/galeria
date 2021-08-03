d7fd3399d0e7fb640fe6b57fc6ff485478090b2c
Diego Laurenti <diegolaurenti13@gmail.com>
2021-08-03 09:44:10 -0300

    commit

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+function abrirModal(){
+    document.getElementById('modal').style.top="0";
+    document.getElementById('modal').style.display="block";
+}
+function fecharModal(){
+    document.getElementById('modal').style.top="-100%";
+    document.getElementById('modal2').style.top="-100%";
+    document.getElementById('modal3').style.top="-100%";
+    document.getElementById('modal4').style.top="-100%";
+    document.getElementById('modal5').style.top="-100%";
+
+}
+
+
+function abrirModal2(){
+    document.getElementById('modal2').style.top="0";
+    document.getElementById('modal2').style.display="block";
+}
+
+function abrirModal3(){
+    document.getElementById('modal3').style.top="0";
+    document.getElementById('modal3').style.display="block";
+}
+
+function abrirModal4(){
+    document.getElementById('modal4').style.top="0";
+    document.getElementById('modal4').style.display="block";
+}
+
+function abrirModal5(){
+    document.getElementById('modal5').style.top="0";
+    document.getElementById('modal5').style.display="block";
+}
